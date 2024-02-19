1fbe96d1ef9d19d49c1ed871b7dcdb466cc50911
JuanLBEnjoyer <juanp.galeanoc@uqvirtual.edu.co>
2024-02-19 17:17:27 -0500

    AÑADIDO

--- src/main/java/co/edu/uniquindio/poo/InversionDeDependencias.java
+++ src/main/java/co/edu/uniquindio/poo/InversionDeDependencias.java
@@ -1,3 +1,56 @@
 package co.edu.uniquindio.poo;
 
+//CORRECTO
+interface CalcularSalario{
+    double calcularSalario();
+}
+abstract class Empleado implements CalcularSalario{
+    private double salarioBase;
+
+    public Empleado(double salarioBase){
+        this.salarioBase = salarioBase;
+    }
+
+    public double calcularSalario(){
+        return salarioBase;
+    }
+}
+
+class Gerente extends Empleado{
+    private double bonoExtra;
+
+    public Gerente(double salarioBase,double bonoExtra){
+        super(salarioBase);
+        this.bonoExtra = bonoExtra;
+    }
+
+    public double calcularSalario(){
+        return super.calcularSalario()+bonoExtra;
+    }
+
+}
+
+class Asistente extends Empleado{
+    public Asistente(double salarioBase){
+        super(salarioBase);
+    }
+}
+
+//INCORRECTO
+
+
+class Gerente {
+    private double bonoExtra;
+    private Empleado empleado;
+
+    public Gerente(double salarioBase, double bonoExtra){
+        super(salarioBase);
+        this.bonoExtra = bonoExtra;
+    }
+
+    public double calcularSalario(){
+        return empleado.calcularSalario() + bonoExtra;
+    }
+}
+
 
